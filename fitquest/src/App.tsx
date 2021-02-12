f925e0c633e89b22b33cb6acfb0ef1b799f82802
Kevin Kantarama <42340855+kevinkant@users.noreply.github.com>
2021-02-12 19:22:01 +0100

    Tab navigation

--- fitquest/src/App.tsx
+++ fitquest/src/App.tsx
@@ -9,10 +9,10 @@ import {
   IonTabs,
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import { barbell, fastFoodOutline, homeOutline, personCircleOutline, starOutline } from 'ionicons/icons';
+import Tab1 from './pages/Tabs/Tab1';
+import Tab2 from './pages/Tabs/Tab2';
+import Tab3 from './pages/Tabs/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -47,22 +47,36 @@ const App: React.FC = () => (
           <Route path="/tab3">
             <Tab3 />
           </Route>
+          <Route path="/tab4">
+            <Tab3 />
+          </Route>
+          <Route path="/tab5">
+            <Tab3 />
+          </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
           </Route>
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon icon={homeOutline} />
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonIcon icon={barbell} />
+            <IonLabel>Workouts</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonIcon icon={personCircleOutline} />
+            <IonLabel>Profile</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab4" href="/tab4">
+            <IonIcon icon={starOutline} />
+            <IonLabel>Achievements</IonLabel>
+          </IonTabButton>
+          <IonTabButton tab="tab5" href="/tab5">
+            <IonIcon icon={fastFoodOutline} />
+            <IonLabel>Diet</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
